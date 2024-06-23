8dc31dd91db666515494b7d2abc1e31b9eb64232
WAHU0508 <sharongikenye@gmail.com>
2024-06-23 15:08:29 +0300

    Create and insert DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//test1
+const element = document.querySelector("main");
+element.remove();
+
+//test2
+const newHeader = document.createElement("h1");
+
+//test3
+newHeader.id = "victory";
+
+//test4
+newHeader.innerHTML = "Sharon is the champion"
